c250f1678f418defdb27eec7851a18860fd65a1e
honyacho <cc_called_pizza@yahoo.co.jp>
2015-09-30 03:03:57 +0900

    remove needless comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,28 @@
 package com.github.honyacho.toho
 
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.By
-import java.util.Date
-import collection.JavaConversions._
+import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.support.ui._
+
+import java.util.Date
+import scala.collection.JavaConversions._
 
 object Main {
   def main(args: Array[String]) {
 
     val target = "17:20"
-
     val driver = new FirefoxDriver()
-    driver.get("http://www.google.co.jp/")
 
     driver.get("https://hlo.tohotheater.jp/net/schedule/032/TNPI2000J01.do")
-    while(System.currentTimeMillis() <= 1443538800000L){
+
+    // starts on 9/30 0:00
+    while (System.currentTimeMillis() <= 1443538800000L) {
       println("まだ")
       println((new Date()).toString)
       Thread.sleep(333)
     }
-    // driver.findElement(By.cssSelector(".sun>.item>.schedule_list_1-date")).click()
-    // driver.findElement(By.cssSelector(".sun_active")).click()
-    // driver.findElements()
+
     driver.findElements(By.cssSelector(".schedule_list_1-date")).filter(_.getText.contains("10/4")).foreach(_.click())
 
     val wait = new WebDriverWait(driver, 15);
@@ -35,7 +34,7 @@ object Main {
               val res = e.getText.contains(target)
               println(res)
               res
-            } catch {case _ => false}
+            } catch { case _ => false }
           })
           .find(e => {
             try {
@@ -55,14 +54,12 @@ object Main {
     driver.findElement(By.cssSelector("#B-17")).click()
     driver.findElement(By.cssSelector("#B-18")).click()
     driver.findElement(By.cssSelector("#bo-navi2>a")).click()
-    // driver.findElements(By.cssSelector(".schedule_table_3-cell_2")).filter(elem =>
-    //   elem.getText.contains("13:50～16:05")
-    // )
 
     println("Type \"close\" if you want to close browser.")
-    while(readLine() != "close"){
+    while (readLine() != "close") {
       println("Type \"close\" if you want to close browser.")
     }
+
     driver.quit()
   }
 }
